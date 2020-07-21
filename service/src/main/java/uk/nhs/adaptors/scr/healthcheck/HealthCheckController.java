16d3a531e02194f3a6e1f869a2f4985c41f07455
Dawid Richert <dawidr@kainos.com>
2020-07-21 09:36:59 +0200

    NIAD-435: Added SpringBoot app skeleton

---
+++ service/src/main/java/uk/nhs/adaptors/scr/healthcheck/HealthCheckController.java
@@ -0,0 +1,18 @@
+package uk.nhs.adaptors.scr.healthcheck;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@AllArgsConstructor
+@Slf4j
+public class HealthCheckController {
+
+    @GetMapping("/healthcheck")
+    public String get() {
+        return "Service is working!";
+    }
+}
